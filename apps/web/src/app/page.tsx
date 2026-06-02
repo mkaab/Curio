4439ebe16dae717ca7b7daf45277f8934508bc15
mkaab <mkaab23@gmail.com>
2026-06-02 17:20:32 +0500

    chore

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -209,7 +209,7 @@ export default function Home() {
                 alt="Sell on Curio"
                 fill
                 priority
-                quality={100}
+                unoptimized={true}
                 sizes="100vw"
                 className="object-cover object-[center_35%]"
               />
@@ -255,7 +255,7 @@ export default function Home() {
                 alt="Sell on Curio"
                 fill
                 priority
-                quality={100}
+                unoptimized={true}
                 sizes="100vw"
                 className="object-cover object-[center_35%] transition-transform duration-1000 group-hover:scale-105"
               />
